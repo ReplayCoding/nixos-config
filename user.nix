d2cc7f2435b1609e92cfe06ebec57d6832bb04e5
ReplayCoding <replaycoding@gmail.com>
2021-10-19 12:22:02 -0700

    set zsh to ignore some commands

--- user.nix
+++ user.nix
@@ -92,6 +92,7 @@ in {
         autocd = true;
         defaultKeymap = "emacs";
         dotDir = ".config/zsh";
+        history.ignorePatterns = [ "ls" "clear" "celar" /* common typo */ ]; # I have a bad habit of spamming these commands
       };
 
       htop = {
