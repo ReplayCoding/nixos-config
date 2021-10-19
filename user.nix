33480c2e26a984a78c1bc2f3226e673b30a7af64
ReplayCoding <replaycoding@gmail.com>
2021-10-19 12:22:02 -0700

    replace custom zsh plugin with built-in home-manager option for syntax highlighting

--- user.nix
+++ user.nix
@@ -73,6 +73,7 @@ in {
         enable = true;
 	enableAutosuggestions = true;
 	enableCompletion = true;
+        enableSyntaxHighlighting = true;
 	autocd = true;
 	defaultKeymap = "emacs";
 	dotDir = ".config/zsh";
@@ -82,17 +83,6 @@ in {
 	envExtra = ''
           alias ls="ls --color=auto"
 	'';
-	plugins = [
-	  {
-	    name = "zsh-syntax-highlighting";
-	    src = pkgs.fetchFromGitHub {
-	      owner = "zsh-users";
-	      repo = "zsh-syntax-highlighting";
-	      rev = "0.7.1";
-	      sha256 = "03r6hpb5fy4yaakqm3lbf4xcvd408r44jgpv4lnzl9asp4sb9qc0";
-	    };
-	  }
-	];
       };
 
       htop = {
