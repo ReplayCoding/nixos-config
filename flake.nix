5a0b01428c3f62c446f137ecb20b7ddf72752794
ReplayCoding <replaycoding@gmail.com>
2021-10-19 12:22:01 -0700

    minor formatting

--- flake.nix
+++ flake.nix
@@ -31,14 +31,8 @@
           home-manager.nixosModules.home-manager
           { home-manager.useGlobalPkgs = true; }
           {
-            nix = {
-              binaryCaches = [
-                "https://nix-community.cachix.org"
-              ];
-              binaryCachePublicKeys = [
-                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-              ];
-            };
+            nix.binaryCaches = [ "https://nix-community.cachix.org" ];
+            nix.binaryCachePublicKeys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
             nixpkgs.overlays = [ neovim-nightly-overlay.overlay ];
           }
           ./user.nix
