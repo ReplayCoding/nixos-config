7ac1cbc75d21d5866823bd232c982a2fd2452a3d
ReplayCoding <replaycoding@gmail.com>
2021-11-07 13:43:40 -0800

    fix: set correct icon theme

--- user/default.nix
+++ user/default.nix
@@ -30,15 +30,15 @@
       enable = true;
       /* theme.package = pkgs.dracula-theme; */
       /* theme.name = "Dracula"; */
-      theme.package = pkgs.gnome.adwaita-icon-theme;
+      theme.package = pkgs.gnome.gnome-themes-extra;
       theme.name = "Adwaita";
-      iconTheme.package = pkgs.gnome.adwaita-icon-theme;
+      iconTheme.package = pkgs.gnome.gnome-themes-extra;
       iconTheme.name = "Adwaita-dark";
       gtk2.extraConfig = "gtk-application-prefer-dark-theme = \"true\"";
       gtk3.extraConfig = { gtk-application-prefer-dark-theme = true; };
     };
 
-    xsession.pointerCursor.package = pkgs.gnome.adwaita-icon-theme;
+    xsession.pointerCursor.package = pkgs.gnome.gnome-themes-extra;
     xsession.pointerCursor.name = "Adwaita";
   };
 }
