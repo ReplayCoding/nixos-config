59851187e19469a0d6a285390cf84df582d32448
ReplayCoding <replaycoding@gmail.com>
2021-11-14 06:47:41 -0800

    feat: make sway output to the journal

--- user/system/wayland.nix
+++ user/system/wayland.nix
@@ -10,7 +10,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.cage}/bin/cage -sd -- ${pkgs.greetd.gtkgreet}/bin/gtkgreet -c sway";
+        command = "${pkgs.cage}/bin/cage -sd -- ${pkgs.greetd.gtkgreet}/bin/gtkgreet -c \"systemd-cat sway\"";
       };
     };
   };
