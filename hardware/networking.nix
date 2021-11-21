f547aa8102554f91c88dc1eafd330758d67b461b
ReplayCoding <replaycoding@gmail.com>
2021-11-20 16:03:42 -0800

    feat: switch back to broadcom-sta drivers
    b43 drivers are slower and randomly disconnect

--- hardware/networking.nix
+++ hardware/networking.nix
@@ -1,11 +1,5 @@
 { config, pkgs, lib, ... }:
 {
-  boot.kernelModules = [ "b43" ];
-  boot.blacklistedKernelModules = [ "wl" ];
-  boot.extraModulePackages = lib.mkForce [ ];
-
-  networking.enableB43Firmware = true;
-
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
