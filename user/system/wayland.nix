f0f6241391a27e3e9e8fac670a6f721ca3cbc852
ReplayCoding <replaycoding@gmail.com>
2021-11-26 09:49:09 -0800

    feat: add gtk desktop portal

--- user/system/wayland.nix
+++ user/system/wayland.nix
@@ -3,8 +3,12 @@
 {
   programs.sway.enable = true; # without this, swaylock will not work.
   programs.sway.extraPackages = lib.mkForce [ ];
-  xdg.portal.enable = true;
-  xdg.portal.wlr.enable = true;
+  xdg.portal = {
+    enable = true;
+    gtkUsePortal = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    wlr.enable = true;
+  };
 
   services.greetd = {
     enable = true;
