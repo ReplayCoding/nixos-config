f30938bd46a598d49689cfc167d3e95575be2359
ReplayCoding <replaycoding@gmail.com>
2021-11-27 13:11:50 -0800

    feat: increase foot scrollback to 10000

--- user/programs/foot.nix
+++ user/programs/foot.nix
@@ -9,6 +9,7 @@ _:
         font = "monospace:size=7.5, emoji";
         dpi-aware = "yes";
       };
+      scrollback.lines = 10000;
       mouse.hide-when-typing = "yes";
       tweak = {
         grapheme-shaping = "yes";
