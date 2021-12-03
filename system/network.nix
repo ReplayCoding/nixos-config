523185ccea430c52c4469da1aee2ebb2336244fb
ReplayCoding <replaycoding@gmail.com>
2021-12-03 11:10:58 -0800

    feat: set nameservers to cloudflare

--- system/network.nix
+++ system/network.nix
@@ -7,12 +7,25 @@
     useNetworkd = true;
     wireless.iwd.enable = true;
 
+    nameservers = [
+      "1.1.1.1#cloudflare-dns.com"
+      "1.0.0.1#cloudflare-dns.com"
+      "2606:4700:4700::1111#cloudflare-dns.com"
+      "2606:4700:4700::1001#cloudflare-dns.com"
+    ];
+
     firewall.allowedTCPPorts = [ ];
     firewall.allowedUDPPorts = [ ];
     firewall.enable = true;
   };
 
   systemd.network.enable = true;
+  services.resolved = {
+    enable = true;
+    extraConfig = ''
+      DNSOverTLS=opportunistic
+    '';
+  };
 
   programs.bandwhich.enable = true;
 }
