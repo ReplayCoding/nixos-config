9a70376450e78d7e6d6ed608e123a3354f71088e
ReplayCoding <replaycoding@gmail.com>
2021-12-06 09:51:35 -0800

    feat: autologin on boot

--- system/greetd.nix
+++ system/greetd.nix
@@ -1,12 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   services.greetd = {
     enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.cage}/bin/cage -sd -- ${pkgs.greetd.gtkgreet}/bin/gtkgreet -c \"systemd-cat sway\"";
+    settings =
+      let cmd = "systemd-cat sway";
+      in
+      {
+        default_session = {
+          command = "${pkgs.cage}/bin/cage -sd -- ${pkgs.greetd.gtkgreet}/bin/gtkgreet -c \"${cmd}\"";
+        };
+        initial_session = {
+          user = "${config.users.users.user.name}";
+          command = "${cmd}";
+        };
       };
-    };
   };
 }
