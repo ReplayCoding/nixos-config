b85e85d7b8f2a8552b64841b99c3f14b9546b123
ReplayCoding <replaycoding@gmail.com>
2021-12-06 18:54:52 -0800

    feat(i3status-rust): show signal strength in network block

--- user/programs/i3status-rust.nix
+++ user/programs/i3status-rust.nix
@@ -28,7 +28,7 @@ in
         (makeMusicBlock "ncspot")
         {
           block = "net";
-          format = "{ip} {ssid}";
+          format = "{ip} {ssid} ({signal_strength})";
           interval = 5;
         }
         {
