a7eb50cc64d1d89a672eb37f33f388619e7ad931
ReplayCoding <replaycoding@gmail.com>
2021-12-14 11:45:38 -0800

    feat(sway): add patch to Implement startup notifications for workspace matching

--- overlays/sway.nix
+++ overlays/sway.nix
@@ -4,7 +4,17 @@
   nixpkgs.overlays = [
     (self: super: {
       sway-unwrapped = super.sway-unwrapped.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [ ./sway-dbus-menus.patch ];
+        patches = (old.patches or [ ]) ++
+          [
+            # Implement startup notifications for workspace matching
+            # https://github.com/swaywm/sway/pull/6639
+            ./sway-6639.patch
+
+            # Tray D-Bus Menu
+            # https://github.com/swaywm/sway/pull/6249
+            ./sway-6249.patch
+            ./sway-fix-compilation-error-in-swaybar.patch
+          ];
       });
     })
   ];
