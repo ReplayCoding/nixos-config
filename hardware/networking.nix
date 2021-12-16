6c0c90b3b4aecfe11963c7fd12fff15ec0487b9d
ReplayCoding <replaycoding@gmail.com>
2021-12-16 12:56:05 -0800

    feat: randomise MAC address

--- hardware/networking.nix
+++ hardware/networking.nix
@@ -3,14 +3,19 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
+  networking = {
+    useDHCP = false;
+    wireless.iwd.settings = {
+      General.AddressRandomization = "once";
+    };
+  };
   systemd.network.networks = {
-    "wired" = {
-      matchConfig.PermanentMACAddress = "c4:54:44:6c:b4:d9";
+    "generic" = {
+      matchConfig.Name = "*";
       DHCP = "yes";
     };
     "wireless" = {
-      matchConfig.PermanentMACAddress = "b8:ee:65:2c:91:65";
+      matchConfig.Type = "wlan";
       DHCP = "yes";
     };
   };
