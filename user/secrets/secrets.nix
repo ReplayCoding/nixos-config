7668ba1492a69b33533e115c2f77634c2cb6c2e8
ReplayCoding <replaycoding@gmail.com>
2021-12-28 11:03:30 -0500

    fix: rekey secrets with librem ssh key

--- user/secrets/secrets.nix
+++ user/secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHn1NkYoDtEQTBRyrltqBQRN4XcuR8nae5l/cXXu3qmD";
-  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdg6DUkevCSHbWOiodfJsBrwiX6HWpdY/yGKfuMe+zX";
-  all = [ user system ];
+  thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdg6DUkevCSHbWOiodfJsBrwiX6HWpdY/yGKfuMe+zX";
+  librem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzFLr8qwNGUgB4j5oSJEPfSSQeueWBNycXikYbalXMy";
+  all = [ user thinkpad librem ];
 in
 {
   "work-email-password.age".publicKeys = all;
