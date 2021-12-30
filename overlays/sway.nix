e7dcd6be38a2be92dac070a6a9537bb52725548b
ReplayCoding <replaycoding@gmail.com>
2021-12-30 14:48:10 -0500

    fix: temporarily disable sway startup notifications patch

--- overlays/sway.nix
+++ overlays/sway.nix
@@ -6,7 +6,8 @@ self: super:
       [
         # Implement startup notifications for workspace matching
         # https://github.com/swaywm/sway/pull/6639
-        ./sway-6639.patch
+        # Broken for Xwayland applications :|
+        # ./sway-6639.patch
 
         # Tray D-Bus Menu
         # https://github.com/swaywm/sway/pull/6249
