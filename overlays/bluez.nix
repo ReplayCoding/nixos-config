29927df9df0137d450e16ed41712cb06cccd4348
ReplayCoding <replaycoding@gmail.com>
2022-01-05 17:08:12 -0800

    fix: remove unused code from bluez override

--- overlays/bluez.nix
+++ overlays/bluez.nix
@@ -1,7 +1,5 @@
 self: super:
 
-let rev = "6a4e37a0f66e21be453e392b37d92647a91f2703";
-in
 {
   bluez5-experimental = super.bluez5.overrideAttrs (old: {
     configureFlags = old.configureFlags ++ [
