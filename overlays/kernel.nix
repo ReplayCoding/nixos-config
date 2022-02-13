e84f8d60d29c24bad916660e56460d05e61387de
ReplayCoding <replaycoding@gmail.com>
2022-02-13 04:03:18 -0800

    Revert "fix: remove LTOed kernel"
    This reverts commit 66f98e93ead911d5c4f440830556b7173c629114.
    It turns out that it was NOT the problem. USB flash drives are just
    really slow. :P

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,6 +1,67 @@
-final: prev: {
+final:
+let
+  inherit (final) lib linuxKernel;
+  inherit (lib.kernel) yes no;
+
+  applyCfg = config: kernel: kernel.override {
+    argsOverride.kernelPatches = kernel.kernelPatches;
+    argsOverride.structuredExtraConfig = kernel.structuredExtraConfig // config;
+  };
+
+  applyLLVM = kernel:
+    let
+      llvmPackages = "llvmPackages_13";
+      noBintools = { bootBintools = null; bootBintoolsNoLibc = null; };
+      hostLLVM = final.buildPackages.${llvmPackages}.override noBintools;
+      buildLLVM = final.${llvmPackages}.override noBintools;
+
+      mkLLVMPlatform = platform: platform // {
+        useLLVM = true;
+        linux-kernel = platform.linux-kernel // {
+          makeFlags = (platform.linux-kernel.makeFlags or [ ]) ++ [
+            "LLVM=1"
+            "LLVM_IAS=1"
+            "CC=${buildLLVM.clangUseLLVM}/bin/clang"
+            "LD=${buildLLVM.lld}/bin/ld.lld"
+            "HOSTLD=${hostLLVM.lld}/bin/ld.lld"
+            "AR=${buildLLVM.llvm}/bin/llvm-ar"
+            "HOSTAR=${hostLLVM.llvm}/bin/llvm-ar"
+            "NM=${buildLLVM.llvm}/bin/llvm-nm"
+            "STRIP=${buildLLVM.llvm}/bin/llvm-strip"
+            "OBJCOPY=${buildLLVM.llvm}/bin/llvm-objcopy"
+            "OBJDUMP=${buildLLVM.llvm}/bin/llvm-objdump"
+            "READELF=${buildLLVM.llvm}/bin/llvm-readelf"
+            "HOSTCC=${hostLLVM.clangUseLLVM}/bin/clang"
+            "HOSTCXX=${hostLLVM.clangUseLLVM}/bin/clang++"
+          ];
+        };
+      };
+      stdenvClangUseLLVM = final.overrideCC hostLLVM.stdenv hostLLVM.clangUseLLVM;
+      stdenvPlatformLLVM = stdenvClangUseLLVM.override (old: {
+        hostPlatform = mkLLVMPlatform old.hostPlatform;
+        buildPlatform = mkLLVMPlatform old.buildPlatform;
+      });
+      stdenv = stdenvPlatformLLVM // {
+        passthru = (stdenvPlatformLLVM.passthru or { }) // { llvmPackages = buildLLVM; };
+      };
+    in
+    kernel.override {
+      inherit stdenv;
+      buildPackages = final.buildPackages // { inherit stdenv; };
+      argsOverride.kernelPatches = kernel.kernelPatches;
+      argsOverride.structuredExtraConfig = kernel.structuredExtraConfig;
+    };
+
+  applyLTO = kernel:
+    applyCfg
+      { LTO_NONE = no; LTO_CLANG_FULL = yes; }
+      (applyLLVM kernel);
+
+  inherit (linuxKernel) packagesFor;
+in
+_: {
   myLinuxPackages =
-    prev.linuxKernel.packages.linux_xanmod.extend (
+    (packagesFor (applyLTO linuxKernel.kernels.linux_xanmod)).extend (
       self: super:
         let
           callPackage = final.newScope super;
