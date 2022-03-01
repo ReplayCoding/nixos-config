3812282666cc8d9d59dfa9a02250f81e2c16d4a2
ReplayCoding <replaycoding@gmail.com>
2022-03-01 08:29:38 -0800

    refactor: set PGO cache max age to 2 days

--- hosts/generic/pgo.nix
+++ hosts/generic/pgo.nix
@@ -7,7 +7,7 @@ in
   if (overlayConfig.pgoMode or "off") != "off"
   then
     {
-      systemd.tmpfiles.rules = [ "d ${dir} 0777 - - -" ];
+      systemd.tmpfiles.rules = [ "d ${dir} 0777 - - 2d" ];
       environment = {
         sessionVariables.LLVM_PROFILE_FILE = "${dir}/%p-%h-%m.profraw";
         systemPackages = with pkgs; [ (extract-pgo-data.override { pgoDir = dir; }) ];
