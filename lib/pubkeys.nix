e64c00c9c34d7386e8b9c82e6188ce902d8a1948
ReplayCoding <replaycoding@gmail.com>
2022-03-10 11:37:43 -0800

    chore: add thinkpad root key

--- lib/pubkeys.nix
+++ lib/pubkeys.nix
@@ -4,7 +4,8 @@ rec {
   systems = [ thinkpad librem ];
 
   root_librem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICkafqvoqbMINmwSHzA4xH2cTHwPhFc+ZIj66/5qsgjp root@librem";
-  roots = [ root_librem ];
+  root_thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGrpf2TTd7B3VWcBttr8qcM5LZwlFw2G4hgTC9M/VyJ1 root@thinkpad";
+  roots = [ root_thinkpad root_librem ];
 
   user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHn1NkYoDtEQTBRyrltqBQRN4XcuR8nae5l/cXXu3qmD";
   users = [ user ];
