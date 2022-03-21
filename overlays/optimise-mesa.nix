957789cac44bd054d71eee3c78a9500e9f12e896
ReplayCoding <replaycoding@gmail.com>
2022-03-21 02:34:23 -0700

    feat: optimise mesa with static libdrm

--- overlays/optimise-mesa.nix
+++ overlays/optimise-mesa.nix
@@ -1,9 +1,19 @@
 self: super: let
-  inherit (import ./optimise-utils.nix super) stdenv mesonOptions_pgo llvmPackages fakeExtra createWithBuildIdList;
+  inherit (import ./optimise-utils.nix super) stdenv makeStatic mesonOptions_pgo llvmPackages fakeExtra createWithBuildIdList getDrvName;
   mkOptimisedMesa = pgoMode: {
+    libdrm-optimised =
+      (super.libdrm.override {
+        stdenv = makeStatic stdenv;
+        # This breaks the build
+        withValgrind = false;
+      })
+      .overrideAttrs (mesonOptions_pgo (getDrvName self.mesa-optimised) pgoMode fakeExtra);
     mesa-optimised =
       (super.mesa.overrideAttrs (mesonOptions_pgo null pgoMode fakeExtra)).override (
-        {inherit llvmPackages stdenv;}
+        {
+          inherit llvmPackages stdenv;
+          libdrm = self.libdrm-optimised;
+        }
         // super.nixosPassthru.mesaConfig
       );
   };
