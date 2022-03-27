4e72801ef7ad97d8e1659330c2fc722d1a9cb8d9
ReplayCoding <replaycoding@gmail.com>
2022-03-27 05:02:08 -0700

    fix: add swapfile

--- hosts/librem/kernel.nix
+++ hosts/librem/kernel.nix
@@ -6,6 +6,12 @@
   boot.kernelPackages = pkgs.myLinuxPackages-librem;
   boot.extraModulePackages = with config.boot.kernelPackages; [librem-ec-acpi-dkms];
   boot.initrd.kernelModules = ["i915"];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 8192;
+    }
+  ];
 
   hardware.opengl.extraPackages = with pkgs; [
     vaapiIntel
