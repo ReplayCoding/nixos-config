3a3a67d68ea19cad77e8509a1ddfab932491d8a1
ReplayCoding <replaycoding@gmail.com>
2022-03-30 04:10:58 -0700

    fix: dont enable pgo in mesa when building for 32-bit system

--- overlays/optimise/mesa.nix
+++ overlays/optimise/mesa.nix
@@ -1,7 +1,12 @@
 self: super: let
-  inherit (import ./utils.nix super) stdenv makeStatic mesonOptions_pgo fakeExtra createWithBuildIdList getDrvName llvmPackages_version;
+  inherit (import ./utils.nix super) stdenv makeStatic mesonOptions mesonOptions_pgo fakeExtra createWithBuildIdList getDrvName llvmPackages_version;
   sources = super.callPackage ../_sources/generated.nix {};
-  mkOptimisedMesa = pgoMode: let
+  mkOptimisedMesa = pgoMode': let
+    inherit (super.stdenv) isi686;
+    pgoMode =
+      if isi686
+      then "off"
+      else pgoMode';
     wayland-optimised =
       (super.wayland.override {stdenv = makeStatic stdenv;})
       .overrideAttrs (mesonOptions_pgo (getDrvName self.mesa-optimised) pgoMode (_: {
@@ -22,7 +27,18 @@ self: super: let
       .override (
         {
           inherit stdenv;
-          llvmPackages = super."${llvmPackages_version}";
+          llvmPackages = let
+            llvmPackages = super."${llvmPackages_version}";
+          in
+            llvmPackages
+            // (
+              if isi686
+              then rec {
+                libllvm = llvmPackages.libllvm.override {inherit stdenv;};
+                llvm = libllvm.out // {outputSpecified = false;};
+              }
+              else {}
+            );
           wayland = wayland-optimised;
           libdrm = libdrm-optimised;
         }
