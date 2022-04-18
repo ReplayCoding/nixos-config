b47a5cad79fef2ae236c2083c40032f659fde417
ReplayCoding <replaycoding@gmail.com>
2022-04-18 15:43:36 -0700

    fix: explicitly enable 32-bit mesa drivers

--- hosts/generic/opengl.nix
+++ hosts/generic/opengl.nix
@@ -6,6 +6,7 @@
   hardware.opengl = {
     enable = true;
     driSupport = true;
+    driSupport32Bit = true;
     package = pkgs.mesa-optimised.drivers;
     package32 = pkgs.mesa-optimised-32.drivers;
   };
