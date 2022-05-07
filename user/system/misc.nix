9313db5c467d5ad93584e5284271114ef1e807c3
ReplayCoding <replaycoding@gmail.com>
2022-05-07 11:57:33 -0700

    feat: add gamemoded

--- user/system/misc.nix
+++ user/system/misc.nix
@@ -3,6 +3,7 @@
     dconf.enable = true;
     steam.enable = true;
     fish.enable = true; # Without this, fish doesn't complete man pages
+    gamemode.enable = true;
   };
   services.flatpak.enable = true;
 }
