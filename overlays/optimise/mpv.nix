543de437208bf164c5e81a5e186c1b43c7397fc6
ReplayCoding <replaycoding@gmail.com>
2022-05-11 11:03:39 -0700

    feat: build mpv with pgo

--- overlays/optimise/mpv.nix
+++ overlays/optimise/mpv.nix
@@ -1,54 +1,54 @@
 self: super: let
-  inherit (import ./utils.nix super) stdenv autotoolsOptions genericOptions mesonOptions makeStatic fakeExtra;
+  inherit (import ./utils.nix super) stdenv autotoolsOptions_pgo mesonOptions_pgo makeStatic fakeExtra getDrvName createWithBuildIdList;
   sources = super.callPackage ../_sources/generated.nix {};
+  pgoName = getDrvName self.mpv-unwrapped;
 
-  dav1d =
-    # This cannot be made into a static library, as lld crashes when linking with lto
-    (super.dav1d.override {inherit stdenv;}).overrideAttrs (
-      mesonOptions
-      (old: {inherit (sources.dav1d) src version;})
-    );
+  mkOptimisedMpv = pgoMode: let
+    dav1d =
+      # This cannot be made into a static library, as lld crashes when linking with lto
+      (super.dav1d.override {inherit stdenv;}).overrideAttrs (
+        mesonOptions_pgo pgoName pgoMode "sample"
+        (old: {inherit (sources.dav1d) src version;})
+      );
 
-  ffmpeg =
-    # While ffmpeg is not using autotools, its build system
-    # is close enough to be compatible with it.
-    (super.ffmpeg-full.overrideAttrs (autotoolsOptions (old: {
-      configureFlags = builtins.filter (f: f != "--enable-shared") old.configureFlags;
-      # Disable tests :O
-      checkPhase = null;
-      safeBitstreamReaderBuild = false;
-      postFixup = super.lib.optionalString stdenv.isLinux ''
-        addOpenGLRunpath $out/lib/libavcodec.a
-        addOpenGLRunpath $out/lib/libavutil.a
-      '';
-    })))
-    .override {
-      stdenv = makeStatic stdenv;
-      ffmpeg = super.ffmpeg_5;
-      inherit dav1d;
-      # Building these programs takes a looooong time
-      ffmpegProgram = false;
-      ffplayProgram = false;
-      ffprobeProgram = false;
-      qtFaststartProgram = false;
-    };
+    ffmpeg =
+      # While ffmpeg is not using autotools, its build system
+      # is close enough to be compatible with it.
+      (super.ffmpeg-full.overrideAttrs (autotoolsOptions_pgo pgoName pgoMode "sample" (old: {
+        configureFlags = builtins.filter (f: f != "--enable-shared") old.configureFlags;
+        # Disable tests :O
+        checkPhase = null;
+        safeBitstreamReaderBuild = false;
+      })))
+      .override {
+        stdenv = makeStatic stdenv;
+        ffmpeg = super.ffmpeg_5;
+        inherit dav1d;
+        # Building these programs takes a looooong time
+        ffmpegProgram = false;
+        ffplayProgram = false;
+        ffprobeProgram = false;
+        qtFaststartProgram = false;
+      };
 
-  libass =
-    (super.libass.override {stdenv = makeStatic stdenv;}).overrideAttrs (autotoolsOptions fakeExtra);
+    libass =
+      (super.libass.override {stdenv = makeStatic stdenv;}).overrideAttrs (autotoolsOptions_pgo pgoName pgoMode "sample" fakeExtra);
 
-  libplacebo = (super.libplacebo.override {stdenv = makeStatic stdenv;}).overrideAttrs (mesonOptions (old: {
-    inherit (sources.libplacebo) src version;
+    libplacebo = (super.libplacebo.override {stdenv = makeStatic stdenv;}).overrideAttrs (mesonOptions_pgo pgoName pgoMode "sample" (old: {
+      inherit (sources.libplacebo) src version;
 
-    mesonFlags = old.mesonFlags ++ ["-Dunwind=disabled"];
-  }));
-
-  mpv-unwrapped =
-    (super.mpv-unwrapped.override {
-      inherit stdenv ffmpeg libass libplacebo;
-      lua = super.luajit;
-    })
-    .overrideAttrs (autotoolsOptions (old: {
-      inherit (sources.mpv) src version;
-      wafFlags = (old.wafFlags or []) ++ ["--verbose"];
+      mesonFlags = old.mesonFlags ++ ["-Dunwind=disabled"];
     }));
-in {inherit mpv-unwrapped;}
+  in {
+    mpv-unwrapped =
+      (super.mpv-unwrapped.override {
+        inherit stdenv ffmpeg libass libplacebo;
+        lua = super.luajit;
+      })
+      .overrideAttrs (autotoolsOptions_pgo pgoName pgoMode "sample" (old: {
+        inherit (sources.mpv) src version;
+        wafFlags = (old.wafFlags or []) ++ ["--verbose"];
+      }));
+  };
+in
+  createWithBuildIdList super mkOptimisedMpv
