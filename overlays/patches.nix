0e43546d37135bf37a61c2122eabe9df8169d37f
ReplayCoding <replaycoding@gmail.com>
2022-06-05 17:50:15 -0700

    fix: make cmus exit properly when paused

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -12,4 +12,13 @@ self: super: {
         ./patches/sway-6249.patch
       ];
   });
+  cmus = super.cmus.overrideAttrs (old: {
+    patches =
+      (old.patches or [])
+      ++ [
+        # Workaround for #1064
+        # https://github.com/cmus/cmus/pull/1172
+        ./patches/cmus-1172.patch
+      ];
+  });
 }
