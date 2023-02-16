27c8eb4574defccbf5a915f4626869e1ec9766fd
ReplayCoding <replaycoding@gmail.com>
2023-02-16 10:53:46 -0800

    fix: rekey secrets

--- lib/pubkeys.nix
+++ lib/pubkeys.nix
@@ -1,14 +1,10 @@
 rec {
   thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdg6DUkevCSHbWOiodfJsBrwiX6HWpdY/yGKfuMe+zX";
-  librem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzFLr8qwNGUgB4j5oSJEPfSSQeueWBNycXikYbalXMy";
+  librem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNXE6bXe8ba+/mWK3Lw1ht0T1IbDYIHnsAB96Qcgcz0";
   systems = [thinkpad librem];
-
-  root_librem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICkafqvoqbMINmwSHzA4xH2cTHwPhFc+ZIj66/5qsgjp root@librem";
-  root_thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGrpf2TTd7B3VWcBttr8qcM5LZwlFw2G4hgTC9M/VyJ1 root@thinkpad";
-  roots = [root_thinkpad root_librem];
 
   user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHn1NkYoDtEQTBRyrltqBQRN4XcuR8nae5l/cXXu3qmD replaycoding@gmail.com";
   users = [user];
 
-  all = systems ++ users ++ roots;
+  all = systems ++ users;
 }
