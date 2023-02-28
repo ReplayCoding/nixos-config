e7dab0c12744c12c1111227fe314eef418bacb6d
ReplayCoding <replaycoding@gmail.com>
2023-02-27 19:31:11 -0800

    feat: add swapfile

--- hosts/librem/kernel.nix
+++ hosts/librem/kernel.nix
@@ -6,7 +6,18 @@
   boot.kernelPackages = pkgs.myLinuxPackages-librem;
   boot.extraModulePackages = with config.boot.kernelPackages; [librem-ec-acpi-dkms];
   boot.initrd.kernelModules = ["i915"];
-  zramSwap.enable = true;
+  zramSwap = {
+    enable = true;
+    priority = 2;
+  };
+
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 8192;
+      priority = 1;
+    }
+  ];
 
   hardware.opengl.extraPackages = with pkgs; [
     vaapiIntel
