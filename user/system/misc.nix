5fa43e4e6c9b01f8e4e67b1ee961696705445200
ReplayCoding <replaycoding@gmail.com>
2023-04-21 16:44:43 -0700

    feat: add libunwind to steam env
    Titanfall 2 needs it

--- user/system/misc.nix
+++ user/system/misc.nix
@@ -1,7 +1,20 @@
 {
+  pkgs,
+  config,
+  ...
+}: {
   programs = {
     dconf.enable = true;
-    steam.enable = true;
+    steam = {
+      enable = true;
+      package = pkgs.steam.override {
+        extraLibraries = pkgs:
+          with config.hardware.opengl;
+            if pkgs.stdenv.hostPlatform.is64bit
+            then [package pkgs.libunwind] ++ extraPackages
+            else [package32 pkgs.libunwind] ++ extraPackages32;
+      };
+    };
     fish.enable = true; # Without this, fish doesn't complete man pages
     gamemode.enable = true;
   };
